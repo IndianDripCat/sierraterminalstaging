bd3f1b8539851cc354bf0d4099fa3c4600952ec0
IndianDripCat <136687762+IndianDripCat@users.noreply.github.com>
2026-03-13 16:47:50 +1100

    Update app.py

--- app.py
+++ app.py
@@ -56,4 +56,5 @@ def roblox_oauth_callback():
     return f"Roblox account linked: {user_data}"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
